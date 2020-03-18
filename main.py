8f32e202c39c7aecd7b930b167b52db0893cf69e
RonyThatsWho <13895900+RonyThatsWho@users.noreply.github.com>
2020-03-18 01:15:55 -0400

    Input parsing and docs
    Filtering invalid input along with function documentation

--- main.py
+++ main.py
@@ -9,6 +9,30 @@
 """
 
 def main():
+    """
+    Main function takes two strings and determines if one-to-one mapping exists
+    True or False result will print to output
+    No output if arguements incorrect
+
+    Parameters:
+        argv[1] (String): String1, String source to be mapped
+        argv[2] (String): String2, String to be mapped too
+
+    Returns:
+        int:
+            0: successful execution
+           -1: parameters missing or invalid
+    """
+
+    if len(sys.argv) < 3 ^ len(sys.argv) > 3 :
+        print (sys.argv)
+        print ("invalid arguements")
+        return -1
+
+    if len(sys.argv[1]) != len(sys.argv[2]):
+        print ("false")
+        return 0
+    
     return 0
 
 
